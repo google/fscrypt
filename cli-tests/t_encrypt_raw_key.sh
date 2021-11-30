38d6cee5930f8109e8ef72a47a8496c875c49280
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-11-29 20:25:56 -0500

    cmd/fscrypt: read key from stdin
    Fixes #123

--- cli-tests/t_encrypt_raw_key.sh
+++ cli-tests/t_encrypt_raw_key.sh
@@ -27,12 +27,27 @@ show_status()
 	fi
 }
 
-begin "Encrypt with raw_key protector"
+begin "Encrypt with raw_key protector from file"
 head -c 32 /dev/urandom > "$raw_key_file"
 fscrypt encrypt --quiet --name=prot --source=raw_key --key="$raw_key_file" "$dir"
 show_status true
 
-begin "Try to encrypt with raw_key protector, using wrong key length"
+begin "Encrypt with raw_key protector from stdin"
+head -c 32 /dev/urandom | fscrypt encrypt --quiet --name=prot --source=raw_key "$dir"
+show_status true
+
+begin "Try to encrypt with raw_key protector from file, using wrong key length"
 head -c 16 /dev/urandom > "$raw_key_file"
 _expect_failure "fscrypt encrypt --quiet --name=prot --source=raw_key --key='$raw_key_file' '$dir'"
 show_status false
+
+begin "Try to encrypt with raw_key protector from stdin, using wrong key length"
+_expect_failure "head -c 16 /dev/urandom | fscrypt encrypt --quiet --name=prot --source=raw_key '$dir'"
+show_status false
+
+begin "Encrypt with raw_key protector from file, unlock from stdin"
+head -c 32 /dev/urandom > "$raw_key_file"
+fscrypt encrypt --quiet --name=prot --source=raw_key --key="$raw_key_file" "$dir"
+fscrypt lock "$dir"
+fscrypt unlock --quiet "$dir" < "$raw_key_file"
+show_status true
